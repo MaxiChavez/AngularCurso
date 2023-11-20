ca0518803454dbbe9d2213e9198b47f252c3b948
Maximiliano Chavez <104655710+MaxiChavez@users.noreply.github.com>
2023-11-20 23:59:10 +0100

    Rendering custom components

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-course-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './course-card.component.html',
+  styleUrl: './course-card.component.css',
+})
+export class CourseCardComponent {}
